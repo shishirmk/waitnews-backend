ae22633d06a60cca0a9a1bda0f9f10e0a2fd466a
Thothathri Srinivasan <me@thothathri.com>
2013-04-28 20:05:43 -0700

    Feature : Added waits to search

--- app/models/wait.rb
+++ app/models/wait.rb
@@ -1,5 +1,5 @@
 class Wait < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
-  attr_accessible :wait_time
+  attr_accessible :wait_time, :place_id
 end
--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,11 +1,17 @@
 class PlaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :averagewait
+  attributes :id, :name, :address, :average_wait_time, :latest_wait_time
   #belongs_to :address
   def address
     Address.find_by_id(object.address_id)
   end
 
-  def averagewait # average wait method
-    Averagewait.find_by_id(object.averagewaits.last.id)
+  def average_wait_time # average wait method
+    avgwait = Averagewait.find_by_id(object.averagewaits.last.id)
+    avgwait.present? ? avgwait.average_time : nil
+  end
+
+  def latest_wait_time
+    latest_wait = Wait.find_by_id(object.waits.last.id)
+    latest_wait.present? ? {:wait_time => latest_wait.wait_time, :updated_at => latest_wait.updated_at} : nil
   end
 end
